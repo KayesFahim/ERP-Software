f299e904516539c51564cd81f2acd1cefcd32768
Kayes Fahim <kayes.fuad@northsouth.edu>
2021-11-26 03:39:36 +0600

    Create Salary Sheet

--- Employee/AddEmployee.php
+++ Employee/AddEmployee.php
@@ -592,13 +592,13 @@ echo "0 results";
 			</div>
 			<!-- /Main Wrapper -->
 			<!-- jQuery -->
-			<script src="assets/js/jquery-3.2.1.min.js"></script>
+			<script src="../assets/js/jquery-3.2.1.min.js"></script>
 			<!-- Bootstrap Core JS -->
-			<script src="assets/js/popper.min.js"></script>
-			<script src="assets/js/bootstrap.min.js"></script>
+			<script src="../assets/js/popper.min.js"></script>
+			<script src="../assets/js/bootstrap.min.js"></script>
 			<!-- Slimscroll JS -->
-			<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+			<script src="../assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 			<!-- Custom JS -->
-			<script  src="assets/js/script.js"></script>
+			<script  src="../assets/js/script.js"></script>
 		</body>
 		</html>
--- Salary/CreateSalary.php
+++ Salary/CreateSalary.php
@@ -1,119 +1,57 @@
 <?php
-
 include '../config.php';
 
+$encryption = $_GET['empId'];
+$ciphering = "AES-128-CTR";
+$iv_length = openssl_cipher_iv_length($ciphering);
+$options = 0;
+$decryption_iv  = '1234567891011121';
+$decryption_key  = "FlyFarInterNational";
+$decryption=openssl_decrypt ($encryption, $ciphering, 
+        $decryption_key, $options, $decryption_iv);
 
-//Reciept No
-
-$Reciept_No ="";
-$sql = "SELECT * FROM moneyreciept ORDER BY recieptNo DESC LIMIT 1";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-        $outputString = preg_replace('/[^0-9]/', '', $row["recieptNo"]);
-		$Reciept_No = "RCP-".(int)$outputString + 1 ;									
- }
-} else {
-echo "0 results";
- }
-
-
-
-//Employee Info
-
-$searchvar = $_GET['search'];
-
-
-if(!empty($searchvar)){
-$Customer_Name=" ";
-$Customer_Email=" ";
-$Customer_Address=" ";
-$Customer_Phone=" ";
-$Customer_Id=" ";
-
-
-$sql = "SELECT * FROM customer where CustomerId='$searchvar' or phone='$searchvar' or email='$searchvar'";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-		$Customer_Id = $row["CustomerId"];
-        $Customer_Name = $row["name"];
-        $Customer_Email = $row["email"];
-        $Customer_Address = $row["address"];
-        $Customer_Phone = $row["phone"];     								
-	}
-} else {
-echo "No Result Found";
- }
-}else{
-	$Customer_Name=" ";
-	$Customer_Email=" ";
-	$Customer_Address=" ";
-	$Customer_Phone=" ";
-
-}
-
-
-// Generate PDF
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $comment = $_POST['comment'];
-    $amount = $_POST['amount'];
-    $payWay = $_POST['paymentway'];
-    $payMethod = $_POST['paymentmethod'];
-    $issueDate = date("d/m/Y");
-    $TxId = $_POST['txid']; 
+    $issueDate = $_POST['date'];
+    $deductionDay = $_POST['deduction'];
+    $absentAmount = $_POST['absentamount'];
+    $lunchMeal = $_POST['lunch'];
+    $timesheetmiss = $_POST['tsm'];
+    $advanceSalary = $_POST['advanced'];
+    $lossOfService = $_POST['los'];
 
-/*
-    $mrgenerate = "INSERT INTO `moneyreciept`(
-		`recieptNo`,
-		`createdBy`,
-		`customerId`,
-		`issueDate`,
-		`TxId`,
-		`amount`,
-		`paymentMethod`,
-		`paymentId`,
-		`comment`
-	)
-	VALUES(
-		'$Reciept_No',
-		'[value-2]',
-		'$Customer_Id',
-		'$issueDate',
-		'$TxId',
-		'$amount',
-		'$payWay',
-		'$payMethod',
-		'$comment'
-	)";
-	*/
-	
-	if($payWay == 'bank'){
+    if(isset($issueDate) && isset($deductionDay) && isset($absentAmount)
+     && isset($lunchMeal) && isset($timesheetmiss) && isset($advanceSalary) && isset($lossOfService)){
+        $sql = "INSERT INTO `employee_salary`(
+            `EMP_Id`,
+            `issueDate`,
+            `deductionDay`,
+            `absentAmount`,
+            `lunchMeal`,
+            `timesheetmiss`,
+            `advanceSalary`,
+            `lossOfService`
+        )
+        VALUES(
+            '$decryption',
+            '$issueDate',
+            '$deductionDay',
+            '$absentAmount',
+            '$lunchMeal',
+            '$timesheetmiss',
+            '$advanceSalary',
+            '$lossOfService'
+        )";
 
-
-
-	}elseif($payWay == 'cash'){
-
-
-	}elseif($payWay == 'mobile_banking'){
-		
-
-	}elseif($payWay == 'ssl_commerce'){
-
-
-	}
-
-     /*   
-    if ($conn->query($sqlquery) === TRUE) {
-            $success = "Record inserted successfully";
-    } else {
-            $error = "Error: " . $sqlquery . "<br>" . $conn->error;
+        if (mysqli_query($conn, $sql)) {
+            echo '<script language="javascript">';
+            echo 'alert("Successfully Created"); location.href="SalaryView.php?empId='.$encryption.'"';
+            echo '</script>';
+         } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
     }
 
-*/
-	
-                                                                                       
 }
 
 ?>
@@ -124,7 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-	<title>Add Money Reciept</title>
+	<title>Create Salary</title>
 	<!-- Favicon -->
 	<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
 	<!-- Bootstrap CSS -->
@@ -323,10 +261,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 				<div class="page-header">
 					<div class="row">
 						<div class="col-sm-12">
-							<h3 class="page-title">Money Receipt</h3>
+							<h3 class="page-title">Create Slalary</h3>
 							<ul class="breadcrumb">
-								<li class="breadcrumb-item"><a href="../project.php">Dashboard</a></li>
-								<li class="breadcrumb-item active">Money Receipt</li>
+								<li class="breadcrumb-item"><a href="../dashboard.php">Dashboard</a></li>
+								<li class="breadcrumb-item active">Salary</li>
 							</ul>
 						</div>
 					</div>
@@ -338,10 +276,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 							<div class="col-md-12">
 								<div class="card">
 									<div class="card-header">
-										<h4 class="text-danger card-title">Customer Details</h4>
-										<div class="text-right">
-											<a href="../Customer/AddCustomer.php" class="btn btn-primary"> Add Customer</a>
-									</div>
+										<h4 class="text-danger card-title">Salary Details</h4>
+                                    
+
 									</div>
 									<div class="card-body">
 										<form action="" method="post">
@@ -349,127 +286,66 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 												<div class="col-md-12">
 													<h4 class="card-title">Details</h4>
 													<div class="row">
-													<div class="col-md-4">
+													<div class="col-md-3">
 															<div class="form-group">
-																<label>Reciept No:</label>
-																<input type="text" value="<?php echo $Reciept_No ?>" class="form-control" disabled>
+																<label>EMP ID:</label>
+																<input type="text" value="<?php echo $decryption ?>" class="form-control" disabled>
 															</div>
 														</div>
-														<div class="col-md-4">
+														<div class="col-md-3">
 															<div class="form-group">
-																<label>Name:</label>
-																<input type="text" value="<?php echo $Customer_Name ?>" class="form-control">
+																<label>Issue Date:</label>
+																<input type="date" name="date" class="form-control" required>
 															</div>
 														</div>
-														<div class="col-md-4">
+														<div class="col-md-3">
 															<div class="form-group">
-																<label>Phone: </label>
-																<input type="phone" value="<?php echo $Customer_Phone ?>" class="form-control">
+																<label>Deduction Days: </label>
+																<input type="number" name="deduction" class="form-control" required>
+															</div>
+														</div>
+                                                        <div class="col-md-3">
+															<div class="form-group">
+																<label>Advanced Salary </label>
+																<input type="number" name="advanced" class="form-control" required>
 															</div>
 														</div>
 														
 													</div>
 													<div class="row">
-														<div class="col-md-4">
+														<div class="col-md-3">
 																<div class="form-group">
-																	<label>Email :</label>
-																	<input type="email" value="<?php echo $Customer_Email ?>" class="form-control">
+																	<label>Absent Amount :</label>
+																	<input type="number" name="absentamount" class="form-control" required>
 																</div>
 															</div>
-														<div class="col-md-4">
+														<div class="col-md-3">
 															<div class="form-group">
-																<label>BCC :</label>
-																<input type="email" name="bccemail"  class="form-control">
+																<label>Lunch Meal</label>
+																<input type="number" name="lunch"  class="form-control" required>
 															</div>
 														</div>
 
-														<div class="col-md-4">
+														<div class="col-md-3">
 															<div class="form-group">
-																<label>Purchase Item Description :</label>
-																<input type="text" name="comment" class="form-control">
+																<label>Time Sheet Missing</label>
+																<input type="number" name="tsm" class="form-control" required>
 															</div>
 														</div>
+                                                        <div class="col-md-3">
+															<div class="form-group">
+																<label>Loss Of Service</label>
+																<input type="number" name="los" class="form-control" required>
+															</div>
+														</div>
+                                                    </div>
 												
-
-
-													</div>
-													<div class="row">
-														<div class="col-md-3">
-																<div class="form-group">
-																	<label>Amount :</label>
-																	<input type="number" name="amount" class="form-control">
-																</div>
-															</div>
-
-														<div class="col-md-3">
-															<div class="form-group">
-																<label>Attachement :</label>
-																<input type="file" name="amount" class="form-control">
-															</div>
-														</div>
-
-														<div class="col-md-6">
-															<div class="form-group">
-																<label>Address :</label>
-																<input type="text" value="<?php echo $Customer_Address ?>" class="form-control">
-															</div>
-														</div>
-														
-													</div>
-													<div class="row">
-														<div class="col-md-4">
-															<div class="form-group">
-																<label>Payment Method:</label>
-																<div class="form-group row">
-																	<div class="col-lg-12">																	
-																	<select name="paymentway" class="select form-control">
-																		<option value="" disabled selected>Select Payment Way</option>
-																		<option value="cash">Cash</option>
-																		<option value="bank">Bank</option>
-																		<option value="ssl_commerce">SSL_commerce</option>
-																		<option value="mobile_banking">Mobile Banking</option>
-																	</select>
-
-																	</div>
-																</div>
-															</div>
-														</div>
-														<div class="col-md-4">
-															<div class="form-group">
-																<label>Payment A/C:</label>
-																<div class="form-group row">
-																	<div class="col-lg-12">
-																	<select name="paymentmethod" class="select form-control">
-																		<option value="" disabled selected>Select Payment Method</option>
-																		<option value="cash">Cash</option>
-																		<option value="ssl_commerce">SSL_commerce</option>
-																		<option value="city">City Bank Limited</option>	
-																		<option value="brac">Brac Bank </option>	
-																		<option value="islami">Islami Bank</option>	
-																		<option value="sonali">Sonali Bank</option>	
-																		<option value="dutch">Dutch Bangla Bank</option>	
-																		<option value="commercial">Commercial Bank</option>	
-																		<option value="ncc">NCC Bank</option>	
-																		<option value="modhumoti">Modhumoti Bank</option>	
-																		<option value="mobile_banking">Mobile Banking</option>
-																		
-
-																	</select>
-																	</div>
-																</div>
-															</div>
-														</div>
-														<div class="col-md-4">
-															<div class="form-group">
-																<label>Reference No:</label>
-																<input type="text" name="txid" class="form-control">
-															</div>
-														</div>
-													</div>
 												</div>
+													
+											
 											</div>
 											<div class="text-right">
-												<button type="submit" class="btn btn-primary"> Generate</button>
+												<button type="submit" class="btn btn-primary"> Create</button>
 											</div>
 										</form>
 									</div>
@@ -477,64 +353,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 							</div>
 						</div>
 					</div>
-					
-
-					<div class="col-md-12">
-							<div class="card">
-								<div class="card-header">
-									<h4 class="card-title">Expense Detail</h4>
-								</div>
-								<div class="card-body">
-
-									<div class="table-responsive">
-										<table class="datatable table table-stripped">
-											<thead>
-												<tr>
-													<th>Reciept No:</th>
-													<th>Issue Date</th>
-													<th>Amount</th>
-													<th>Created By</th>
-													<th>Customer</th>
-                                                    <th>Payment Method</th>
-													
-                                                    <th>Action</th>
-												</tr>
-											</thead>
-											<tbody>
-											<?php
-
-													$sql = "SELECT recieptNo, createdBy, customerId, issueDate, TxId, amount, paymentMethod, paymentId   FROM `moneyreciept` ORDER BY id DESC LIMIT 5";
-													$result = $conn->query($sql);
-
-													if ($result->num_rows > 0) {
-													while($row = $result->fetch_assoc()) {	
-														$Rno = $row["recieptNo"];
-														echo "<tr><td>".$row["recieptNo"]."</td>
-																	<td>".$row["issueDate"]."</td> 
-																	<td>".$row["amount"]."</td>
-																	<td>".$row["createdBy"]."</td>
-																	<td>".$row["customerId"]."</td>
-																	<td>".$row["paymentMethod"]."</td>
-																	<td><a href='Invoice.php?Rno=$Rno' class='btn btn-primary'> <i class='fe fe-print'></i> </a>
-																	<a href='Invoice.php?Rno=$Rno' class='btn btn-success'> <i class='fe fe-edit'></i> </a>
-																	<a href='Invoice.php?Rno=$Rno' class='btn btn-danger'> <i class='fe fe-trash'></i> </a>
-																	<a href='Invoice.php?Rno=$Rno' class='btn btn-danger'> <i class='fe fe-mail'></i> </a>
-																	</tr>";   											
-													}
-													} else {
-													echo "0 results";
-													}
-													?>
-                                                    </td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-								</div>
-							</div>
-						</div>
-                    </div>
-										
+															
 						<!-- End Contant -->
 					</div>			
 				</div>
@@ -563,16 +382,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			});
 			</script>
 			<!-- jQuery -->
-			<script src="../assets/js/jquery-3.2.1.min.js"></script>
+			<script src="assets/js/jquery-3.2.1.min.js"></script>
 			<!-- Bootstrap Core JS -->
-			<script src="../assets/js/popper.min.js"></script>
-			<script src="../assets/js/bootstrap.min.js"></script>
+			<script src="assets/js/popper.min.js"></script>
+			<script src="assets/js/bootstrap.min.js"></script>
 			<!-- Slimscroll JS -->
-			<script src="../assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+			<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 			<!-- Datatables JS -->
-			<script src="../assets/plugins/datatables/jquery.dataTables.min.js"></script>
-			<script src="../assets/plugins/datatables/datatables.min.js"></script>
+			<script src="assets/plugins/datatables/jquery.dataTables.min.js"></script>
+			<script src="assets/plugins/datatables/datatables.min.js"></script>
 			<!-- Custom JS -->
-			<script  src="../assets/js/script.js"></script>
+			<script  src="assets/js/script.js"></script>
 	</body>
 </html>
